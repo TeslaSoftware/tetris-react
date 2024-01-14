00bbbdd3329ef27d801e548e7d3d40052494d5dc
Sebastian Tysler <stysler@gmail.com>
2024-01-14 16:22:30 -0600

    Add scafolding for the components

---
+++ src/components/Tetris.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+//Components
+import Stage from './Stage';
+import Display from './Display';
+import StartButton from './StartButton';
+
+const Tetris = () => {
+
+    return (
+        <div>
+            <Stage />
+            <aside>
+                <div>
+                    <Display  text="Score"></Display>
+                    <Display  text="Rows"></Display>
+                    <Display  text="Level"></Display>
+                </div>
+                <StartButton />
+            </aside>
+        </div>
+    )
+}
+
+export default Tetris;
